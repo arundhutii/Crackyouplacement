258b2dd9239a4913648a1cc18f608b6a6db71727
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-08-05 00:03:11 +0530

    Updated 169._Majority_element.java

--- 169._Majority_element.java
+++ 169._Majority_element.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int majorityElement(int[] nums) {
+        int maj= nums[0], votes= 1;
+        for(int i=1; i<nums.length; i++){
+            if(votes== 0){
+                votes++;
+                maj= nums[i];
+            } else if(maj== nums[i]){
+                votes++;
+            }else
+                votes--;
+        }
+        return maj;
+    }
+}
