2f7b039d55dd4dec919526d85111f9edbf6fd8df
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-09-04 23:56:26 +0530

    Updated 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,13 +1,19 @@
-class Solution:
-  def productExceptSelf(self, nums: list[int]) -> list[int]:
-    n = len(nums)
-    prefix = [1] * n  # prefix product
-    suffix = [1] * n  # suffix product
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda interval: interval[0])
+        merged = []
 
-    for i in range(1, n):
-      prefix[i] = prefix[i - 1] * nums[i - 1]
-
-    for i in reversed(range(n - 1)):
-      suffix[i] = suffix[i + 1] * nums[i + 1]
-
-    return [prefix[i] * suffix[i] for i in range(n)]
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1] = [merged[-1][0], max(merged[-1][1], interval[1])]
+        
+        return merged
+        # Time: O(n log n)
+        # Space: O(n)
+        
