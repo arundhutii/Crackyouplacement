7fd777c881a8cdfd1e6589eb5bd37ab02df871e5
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-09-21 23:28:42 +0530

    Update Best_time_to_buy_and_sell_stock_121.py

--- Best_time_to_buy_and_sell_stock_121.py
+++ Best_time_to_buy_and_sell_stock_121.py
@@ -2,10 +2,14 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_prof=0
         min_price= float('inf')
+
         for price in prices:
-            if price < min_price:
+            
+            if price<min_price:
                 min_price= price
-            profit= price-min_price
-            if profit > max_prof:
-                max_prof=profit
+            profit= price- min_price
+
+            if profit> max_prof:
+                max_prof= profit
+                
         return max_prof
