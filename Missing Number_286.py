c8922502b4734565d3c67488b1ba165232fbfa5b
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-09-26 22:58:19 +0530

    Update Missing Number_286.py

--- Missing Number_286.py
+++ Missing Number_286.py
@@ -1,6 +1,6 @@
 class Solution:
   def missingNumber(self, nums: List[int])-> int:
     res= len(nums)
-    for i in range(len(nums)):
+    for i in range(len(nums)): 
       res += (i- nums[i])
     return res 
