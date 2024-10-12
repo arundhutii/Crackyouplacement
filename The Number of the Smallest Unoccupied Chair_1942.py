98ec0d9d435f57153c272ace53d9270e00a73238
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-10-13 01:44:22 +0530

    Update The Number of the Smallest Unoccupied Chair_1942.py

--- The Number of the Smallest Unoccupied Chair_1942.py
+++ The Number of the Smallest Unoccupied Chair_1942.py
@@ -1,23 +1,24 @@
 class Solution:
-  def smallestChair(self, times: list[list[int]], targetFriend: int) -> int:
-    nextUnsatChair = 0
-    emptyChairs = []
-    occupied = []  # (leaving, chair)
+    def smallestChair(self, times: list[list[int]], targetFriend: int) -> int:
+        indexes = [i for i in range(len(times))]
+        indexes.sort(key=lambda i: times[i][0])
 
-    for i in range(len(times)):
-      times[i].append(i)
+        used_chairs = []  # (leave_time, chair_number) min heap
+        available_chairs = [i for i in range(len(times))]  # min-heap of available chairs
+        
+        heapq.heapify(available_chairs)  
 
-    times.sort(key=lambda x: x[0])
-
-    for arrival, leaving, i in times:
-      while len(occupied) > 0 and occupied[0][0] <= arrival:
-        unsatChair = heapq.heappop(occupied)[1]
-        heapq.heappush(emptyChairs, unsatChair)
-      if i == targetFriend:
-        return emptyChairs[0] if len(emptyChairs) > 0 else nextUnsatChair
-      if len(emptyChairs) == 0:
-        heapq.heappush(occupied, (leaving, nextUnsatChair))
-        nextUnsatChair += 1
-      else:
-        emptyChair = heapq.heappop(emptyChairs)
-        heapq.heappush(occupied, (leaving, emptyChair))
+        for i in indexes:
+            av, leave = times[i]
+            
+            while used_chairs and used_chairs[0][0] <= av:
+                leave_time, chair = heapq.heappop(used_chairs)
+                heapq.heappush(available_chairs, chair) 
+         
+            chair = heapq.heappop(available_chairs)
+            heapq.heappush(used_chairs, (leave, chair))
+            
+            # If this is the target friend, return the chair number
+            if i == targetFriend:
+                return chair
+            
