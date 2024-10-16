f484f45ebd2fc66e3ad87ca01e725ebfffa46772
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-10-16 23:35:36 +0530

    Update Longest Happy String_1405.py

--- Longest Happy String_1405.py
+++ Longest Happy String_1405.py
@@ -8,27 +8,20 @@ class Solution:
         if a > 0: heapq.heappush(maxheap, (-a, 'a'))
         if b > 0: heapq.heappush(maxheap, (-b, 'b'))
         if c > 0: heapq.heappush(maxheap, (-c, 'c'))
-
         while maxheap:
             count, char = heapq.heappop(maxheap)
             n = len(res)
-
-            # Check if the current character has occurred in the previous 2 indices
             if n >= 2 and res[-1] == char and res[-2] == char:
                 if not maxheap:
                     break
-
                 nextCount, nextChar = heapq.heappop(maxheap)
                 res.append(nextChar)
                 nextCount += 1
-
                 if nextCount < 0:
                     heapq.heappush(maxheap, (nextCount, nextChar))
             else:
                 res.append(char)
                 count += 1
-
             if count < 0:
                 heapq.heappush(maxheap, (count, char))
-
         return ''.join(res)
