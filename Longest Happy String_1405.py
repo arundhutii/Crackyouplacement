87cd6c315a988181006a69606cb8cae775f1e00a
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2024-10-16 23:34:41 +0530

    Create Longest Happy String_1405.py

---
+++ Longest Happy String_1405.py
@@ -0,0 +1,34 @@
+import heapq
+
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        res = []
+        maxheap = []
+        
+        if a > 0: heapq.heappush(maxheap, (-a, 'a'))
+        if b > 0: heapq.heappush(maxheap, (-b, 'b'))
+        if c > 0: heapq.heappush(maxheap, (-c, 'c'))
+
+        while maxheap:
+            count, char = heapq.heappop(maxheap)
+            n = len(res)
+
+            # Check if the current character has occurred in the previous 2 indices
+            if n >= 2 and res[-1] == char and res[-2] == char:
+                if not maxheap:
+                    break
+
+                nextCount, nextChar = heapq.heappop(maxheap)
+                res.append(nextChar)
+                nextCount += 1
+
+                if nextCount < 0:
+                    heapq.heappush(maxheap, (nextCount, nextChar))
+            else:
+                res.append(char)
+                count += 1
+
+            if count < 0:
+                heapq.heappush(maxheap, (count, char))
+
+        return ''.join(res)
