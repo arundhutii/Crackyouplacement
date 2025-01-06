3e7451da33dfdf02753d3548e90ceb77589f3892
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2025-01-06 23:26:03 +0530

    Update Longest Common Prefix_14.py

--- Longest Common Prefix_14.py
+++ Longest Common Prefix_14.py
@@ -1,11 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         min_len=float('inf')
-
         for s in strs:
             if len(s)< min_len:
-                min_len= len(s)
-            
+                min_len= len(s)  
         i=0
         while i<min_len:
             for s in strs:
