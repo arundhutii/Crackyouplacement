a0103a3eff39e7106d8aaa7092a51e05ccd655db
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2025-01-19 23:05:22 +0530

    Update Top K Frequent Elements_347.py

--- Top K Frequent Elements_347.py
+++ Top K Frequent Elements_347.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = Counter(nums)
+        return [num for num, freq in count.most_common(k)]
