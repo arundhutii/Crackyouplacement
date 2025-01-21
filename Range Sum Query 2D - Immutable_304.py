9b3738fabf547d32d5575837a75ee0468da817f4
Arundhuti <98275046+arundhutii@users.noreply.github.com>
2025-01-21 22:15:22 +0530

    Update Range Sum Query 2D - Immutable_304.py

--- Range Sum Query 2D - Immutable_304.py
+++ Range Sum Query 2D - Immutable_304.py
@@ -1,1 +1,11 @@
+class NumMatrix:
 
+    def __init__(self, matrix: list[list[int]]):
+        self.matrix = matrix
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        sum = 0
+        for i in range(row1, row2 + 1):
+            for j in range(col1, col2 + 1):
+                sum += self.matrix[i][j]
+        return sum
